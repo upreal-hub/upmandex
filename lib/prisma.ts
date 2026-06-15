9444453e626f9f2af62352ef49650f482bb39e4d
upreal-hub <thefoxthe07@gmail.com>
2026-06-15 13:47:16 +0200

    fix prisma import

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,4 @@
-import "dotenv/config";
-
-import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma =
