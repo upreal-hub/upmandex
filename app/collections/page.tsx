c25a00173ec076d8e697296cea8cf7b489f7e4ea
upreal-hub <thefoxthe07@gmail.com>
2026-06-15 15:31:04 +0200

    debug collection

--- app/collections/page.tsx
+++ app/collections/page.tsx
@@ -4,6 +4,9 @@ import { prisma } from "@/lib/prisma";
 export const dynamic =
   "force-dynamic";
 
+export const revalidate =
+  0;
+
 export default async function CollectionsPage() {
 
   const users =
